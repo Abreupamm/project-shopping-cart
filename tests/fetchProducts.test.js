a18a30601b2a8e261d5df3116dd3435131d75dbe
pamela <abreupamm@gmail.com>
2022-04-27 19:41:23 -0300

    questão 1 feita

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,18 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
   fail('Teste vazio');
+  // it('1 - Verifica se fetchProducts é uma função', () => {
+  //   expect(typeof fetchProducts).toEqual('function')
+  // });
+
+  // it('2 - Verifica se fetch foi chamada quando chamada a função fetchProducts com o argumento "computador"', async () => {
+  //   const products = await fetchProducts('computador');
+  //   // expect(products).toEqual()
+  // });
+
+  // it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', async () => {
+  //   const products = await fetchProducts('computador');
+  //   expect(products).toEqual()
+  // });
 });
