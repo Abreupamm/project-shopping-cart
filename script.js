b8b3fffeb429b24b8c315abcd3de8ceb544d522c
pamela <abreupamm@gmail.com>
2022-04-27 20:56:22 -0300

    questão 1 iniciando testes da questão 8

--- script.js
+++ script.js
@@ -24,21 +24,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código 
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código 
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 const createProducts = async () => {
   const fetch = await fetchProducts('computador');
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,18 +3,28 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  fail('Teste vazio');
-  // it('1 - Verifica se fetchProducts é uma função', () => {
-  //   expect(typeof fetchProducts).toEqual('function')
-  // });
 
-  // it('2 - Verifica se fetch foi chamada quando chamada a função fetchProducts com o argumento "computador"', async () => {
-  //   const products = await fetchProducts('computador');
-  //   // expect(products).toEqual()
-  // });
+  beforeEach(async () => {
+    return products = await fetchProducts('computador');
+  });
+  // fail('Teste vazio');
+  it('1 - Verifica se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
 
-  // it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', async () => {
-  //   const products = await fetchProducts('computador');
-  //   expect(products).toEqual()
+  it('2 - Verifica se fetch foi chamada quando chamada a função fetchProducts com o argumento "computador"', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    const expectedFetch = await fetch(url);
+    const expectedJson = await expectedFetch.json();
+    const expected = expectedJson.results;
+    expect(products).toEqual(expected);
+  });
+
+  it('Verifica se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const expected = typeof computadorSearch;
+    expect(typeof products).toEqual(expected);
+  });
+
+  // it('verifica  se ao chamar a função fetchProducts sem argumento, retorna um erro', async () => {
   // });
 });
